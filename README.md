0444224b1064160d385f2fbf23b1925a9ffa2ec8
Tim Simpson <timsimpson4@gmail.com>
2014-03-21 17:19:00 -0500

    Close to finishing up the site now
    Still need to add some major pages.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-macaroni-site
-=============
+Macaroni for C++ for the Internet
+=================================
 
-Web site for Macaroni
+This is the website for Macaroni. Previously the entire website was stored as
+part of Macaroni's normal repo but this made it difficult to casually edit
+which meant often times I left out information or didn't properly talk about
+which releases were currently available. Now the Macaroni code repo just
+generates some of the docs while this repo stores the rest of the site.
---
+++ project.lua
@@ -0,0 +1,71 @@
+require "os"
+require "Macaroni.IO.Path"
+
+sources = { "source/examples", "source/www" }
+outputPath = "target"
+
+project = context:Group("Macaroni")
+                 :Project("Macaroni.Site")
+                 :Version("DEV")
+
+local lib = project:Library{
+    name="lib",
+    shortName = "site-examples",
+    headers=pathList{ "source/examples" },
+    sources=pathList{ "source/examples" },
+    dependencies = { load("Macaroni", "CppStd", "2003"):Target("lib"),
+                     load("Macaroni", "Boost-headers", "1.55"):Target("lib") }
+}
+
+
+
+local dir = Macaroni.IO.Path.New(getWorkingDirectory())
+
+function newPath(suffix)
+    return dir:NewPathForceSlash(suffix):CreateWithCurrentAsRoot();
+end
+
+
+
+function buildExamples()
+    local porg = plugins:Get("Porg")
+    local cpp = plugins:Get("Cpp")
+
+    porg:Run("Generate", {target=lib});
+
+    result, msg = pcall(function()
+        cpp:Run("Generate", { projectVersion=project,
+                              path=filePath(outputPath)})
+    end)
+    if not result then
+        output:ErrorLine(msg)
+        error(msg)
+    end
+end
+
+function buildWebSite()
+    local site = plugins:Get("Site")
+    site:Run("build", {
+        project=project,
+        outputPath=newPath("target"),
+        sources={newPath("source/www")},
+        output=output,
+        context=context
+    });
+end
+
+function generate()
+end
+
+built = false
+
+function build()
+    if built then return end
+    built = true
+    buildExamples()
+    buildWebSite()
+end
+
+function install()
+    build()
+end
